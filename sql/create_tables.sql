cc77286c58dad2882aec56be89583dc5d8b5be3d
mzl <mzl.lenexa@gmail.com>
2024-09-28 21:34:24 -0500

    file creation 2

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,4 @@
-- Start by deleting any tables if the exist already
+-- Start by deleting any tables if the exist already
 -- We want to be able to re-run this script as needed.
 -- DROP tables in reverse order of creation 
 -- DROP dependent tables (with foreign keys) first
